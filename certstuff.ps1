54190ffab146c4c603d216d94f27a4a299f9ee89
yannovski <iain.robins@gmail.com>
2016-11-14 17:21:19 +0000

    added deploy script

---
+++ certstuff.ps1
@@ -0,0 +1,36 @@
+﻿$certificateName = "MyLab01RemoteAccess"
+
+$thumbprint = (New-SelfSignedCertificate -DnsName $certificateName -CertStoreLocation Cert:\CurrentUser\My -KeySpec KeyExchange).Thumbprint
+
+$cert = (Get-ChildItem -Path cert:\CurrentUser\My\$thumbprint)
+
+$password = ConvertTo-SecureString -AsPlainText "P@55w0rd" -Force
+
+$path = "c:\$certificateName.pfx"
+
+Export-PfxCertificate -Cert $cert -FilePath $path -Password $password
+
+$fileContentBytes = Get-Content $path -Encoding Byte
+$fileContentEncoded = [System.Convert]::ToBase64String($fileContentBytes)
+
+$jsonObject = @"
+{
+  "data": "$filecontentencoded",
+  "dataType" :"pfx",
+  "password": "P@55w0rd"
+}
+"@
+
+$jsonObjectBytes = [System.Text.Encoding]::UTF8.GetBytes($jsonObject)
+$jsonEncoded = [System.Convert]::ToBase64String($jsonObjectBytes)
+
+$secret = ConvertTo-SecureString -String $jsonEncoded -AsPlainText –Force
+
+$VaultName = 'Mylab01KeyVault01'
+$SecretName = 'MyLab01Cert01'
+
+Set-AzureKeyVaultSecret -VaultName $VaultName -Name $SecretName -SecretValue $secret
+
+
+$cred = Get-Credential
+Enter-PSSession -ConnectionUri https://13.94.235.195:5986 -Credential $cred -SessionOption (New-PSSessionOption -SkipCACheck -SkipCNCheck -SkipRevocationCheck) -Authentication Negotiate
---
+++ deploytemplate.ps1
@@ -0,0 +1,65 @@
+﻿Param (
+    [string]$certificateName = "MyLab01RemoteAccess",
+    [string]$certificatePassword = "P@55w0rd",
+    [string]$certificatePath = "c:\",
+    [string]$armTemplatePath = "C:\Source\irobins\MyLab01\MyLab01\azuredeploy.json",
+    [string]$armParamPath = "C:\Source\irobins\MyLab01\MyLab01\azuredeploy.parameters.json",
+    [string]$resourcegroupname = "MyLab01",
+    [string]$certificateStore = "my",
+    [string]$azureregion = 'westeurope'
+)
+
+# Create new self signed cert
+$thumbprint = (New-SelfSignedCertificate -DnsName $certificateName -CertStoreLocation "Cert:\CurrentUser\$certificateStore" -KeySpec KeyExchange).Thumbprint
+
+# Get the new cert
+$certificate = (Get-ChildItem -Path "cert:\CurrentUser\$certificateStore\$thumbprint")
+
+# Convert password param to secure string
+$securePassword = ConvertTo-SecureString -AsPlainText $certificatePassword -Force
+
+# Define path to export certificate to
+$certificatePath = "$certificatePath\$certificateName.pfx"
+
+# Export as PFX (so it has the private key)
+Export-PfxCertificate -Cert $certificate -FilePath $certificatePath -Password $securePassword
+
+# Read the certificate file
+$fileContentBytes = Get-Content $certificatePath -Encoding Byte
+# Convert the bytes to base64
+$fileContentEncoded = [System.Convert]::ToBase64String($fileContentBytes)
+
+# Generate JSON object for the encoded PFX
+$jsonObject = @"
+{
+  "data": $filecontentencoded,
+  "dataType" :"pfx",
+  "password": $securePassword
+}
+"@
+
+# Encode the JSON object
+$jsonObjectBytes = [System.Text.Encoding]::UTF8.GetBytes($jsonObject)
+$jsonEncoded = [System.Convert]::ToBase64String($jsonObjectBytes)
+
+# Convert encoded JSON to secure string
+$secret = ConvertTo-SecureString -String $jsonEncoded -AsPlainText –Force
+
+# Cleanup cert
+Remove-item $certificatePath -Force
+$certificate | Remove-Item
+
+# Check to see if the resource group already exists
+$ResourceGroup = Get-AzureRmResourceGroup -Name $resourcegroupname
+
+# If not, create it
+If ($ResourceGroup -eq $null) {
+    New-AzureRmResourceGroup -Name $resourcegroupname -Location $azureregion
+}
+
+$DeploymentParameters = @{
+    'secretValue' = $secret;
+}
+
+New-AzureRmResourceGroupDeployment -Name ((Get-ChildItem $armTemplatePath).BaseName + '-' + ((Get-Date).ToUniversalTime()).ToString('MMdd-HHmm')) -ResourceGroupName $ResourceGroupName -TemplateFile $armTemplatePath -TemplateParameterFile $armParamPath @DeploymentParameters -Force -Verbose 
+
