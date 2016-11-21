f78c1158826563adbb86108abd6849020f278c3f
yannovski <iain.robins@gmail.com>
2016-11-21 17:26:05 +0000

    Updated TestRunbook1.ps1 in Azure Automation.

--- azurerunbooks/TestRunbook1.ps1
+++ azurerunbooks/TestRunbook1.ps1
@@ -1,1 +1,23 @@
-Get-AzureRMVM
+$connectionName = "AzureRunAsConnection"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Add-AzureRmAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
